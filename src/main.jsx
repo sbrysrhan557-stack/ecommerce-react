27ffae57151c872114843f734a6bb87ebb861adb
Sabry Saleh <sbrysrhan557@gmail.com>
2026-09-08 03:02:50 +0300

    fix: Set base URL for BrowserRouter and update Vite config base path

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { WishlistProvider } from "@/components/context/WishlistContext";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <CardProvider>
         <WishlistProvider>
           <App />
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ecommerce-react/',
   plugins: [
     react(),
     tailwindcss(),
